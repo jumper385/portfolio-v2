c05265e09996fbed7b46c725e9d626819641703b
Henry Chen <henrychen385@gmail.com>
2022-06-08 01:10:13 +0800

    added latex style font 🧑‍🎨🧑‍🎨🧑‍🎨

--- client/src/routes/__layout.svelte
+++ client/src/routes/__layout.svelte
@@ -1,18 +1,31 @@
 <script>
-import Footer from "../components/Footer.svelte";
-import Navbar from "../components/Navbar.svelte";
-
+  import Footer from "../components/Footer.svelte";
+  import Navbar from "../components/Navbar.svelte";
 </script>
+
 <svelte:head>
-    <link rel="stylesheet" href="normalize.css">
-    <link rel="stylesheet" href="global.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="normalize.css" />
+  <link rel="stylesheet" href="global.css" />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+    integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+  />
+  <link
+    rel="stylesheet"
+    href="https://cdn.rawgit.com/dreampulse/computer-modern-web-font/master/fonts.css"
+  />
 </svelte:head>
 
 <Navbar />
 
 <div>
-    <slot></slot>
+  <slot />
 </div>
 
 <Footer />
+
+<style>
+</style>
