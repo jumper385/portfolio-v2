8f4f7d997353713d76c16dc09d290e7bd2ac992e
Henry Chen <henrychen385@gmail.com>
2022-06-08 01:07:56 +0800

    fixed issue #17 with 64pt margin instead

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,6 +1,6 @@
 <script>
-  import {user} from '$lib/sessionStore'
-  import {supabase} from '$lib/supabaseClient'
+  import { user } from "$lib/sessionStore";
+  import { supabase } from "$lib/supabaseClient";
   user.set(supabase.auth.user());
 
   supabase.auth.onAuthStateChange((_, session) => {
@@ -9,45 +9,51 @@
 </script>
 
 <title>Home</title>
-<section class="landing-container">
-  <img
-    class="landing-img"
-    src="/dissasembled-fsk-min.png"
-    alt="exploded view of a pcb module"
-  />
-
-  <div class="landing-description">
-    <h1>chenry</h1>
-    <p>
-      Electronics engineer with 6 years experience in the Biomedical, Aerospace,
-      automotive and Resources industry
-    </p>
-    <a href="/cases">View my Work</a>
-  </div>
-</section>
-
-<section class="profile-description">
-  <img
-    class="profile-picture"
-    src="/profile-picture-square-min.JPG"
-    alt="sum azn guy"
-  />
-  <div>
-    <h1>I'm Henry</h1>
-    <p>Electron Resource Manager at VitalTrace</p>
-    <p>BSc Electrical/Electronics Engineering & Management (UWA)</p>
-    <p>
-      I am a UWA Electrical Engineering and Management undergraduate with six
-      years of experience in electronics (Analog, Embedded RF and Mix Signal),
-      machine learning, web development and mechatronic design. I currently work
-      at VitalTrace as an Undergraduate Electronics Engineer. I seek to combine
-      electronics and industrial design to intuitively enhance the human
-      experience.
-    </p>
-  </div>
-</section>
+<div class="container">
+  <section class="landing-container">
+    <img
+      class="landing-img"
+      src="/dissasembled-fsk-min.png"
+      alt="exploded view of a pcb module"
+    />
+  
+    <div class="landing-description">
+      <h1>chenry</h1>
+      <p>
+        Electronics engineer with 6 years experience in the Biomedical, Aerospace,
+        automotive and Resources industry
+      </p>
+      <a href="/cases">View my Work</a>
+    </div>
+  </section>
+  
+  <section class="profile-description">
+    <img
+      class="profile-picture"
+      src="/profile-picture-square-min.JPG"
+      alt="sum azn guy"
+    />
+    <div>
+      <h1>I'm Henry</h1>
+      <p>Electron Resource Manager at VitalTrace</p>
+      <p>BSc Electrical/Electronics Engineering & Management (UWA)</p>
+      <p style="flex-wrap:wrap">
+        I am a UWA Electrical Engineering and Management undergraduate with six
+        years of experience in electronics (Analog, Embedded RF and Mix Signal),
+        machine learning, web development and mechatronic design. I currently work
+        at VitalTrace as an Undergraduate Electronics Engineer. I seek to combine
+        electronics and industrial design to intuitively enhance the human
+        experience.
+      </p>
+    </div>
+  </section>
+</div>
 
 <style>
+  .container {
+    max-width: 960px;
+    margin:0 auto;
+  }
   .landing-img {
     width: 100%;
   }
