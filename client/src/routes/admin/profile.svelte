dba887b8ae472c26fdbf3a63206699e0c54702a0
Henry Chen <henrychen385@gmail.com>
2022-06-08 21:50:31 +0800

    added password login + improved profile layout #15

--- client/src/routes/admin/profile.svelte
+++ client/src/routes/admin/profile.svelte
@@ -1,105 +1,143 @@
 <script>
-    import { supabase } from '$lib/supabaseClient'
-    import { user, fileUpload } from '$lib/sessionStore'
-  
-    let loading = true
-    let username = null
-    let website = null
-    let avatar_url = null
-  
-    async function getProfile() {
-      try {
-        loading = true
-        const user = supabase.auth.user()
-  
-        let { data, error, status } = await supabase
-          .from('profiles')
-          .select(`username, website, avatar_url`)
-          .eq('id', user.id)
-          .single()
-  
-        if (error && status !== 406) throw error
-  
-        if (data) {
-          username = data.username
-          website = data.website
-          avatar_url = data.avatar_url
-        }
-      } catch (error) {
-        alert(error.message)
-      } finally {
-        loading = false
+  import { supabase } from "$lib/supabaseClient";
+  import { user, fileUpload } from "$lib/sessionStore";
+
+  let profile = {};
+  let password = {};
+  let loading = false;
+
+  async function getProfile() {
+    try {
+      loading = true;
+      const user = supabase.auth.user();
+      console.log(user);
+      let { data, error, status } = await supabase
+        .from("profiles")
+        .select()
+        .eq("id", user.id)
+        .single();
+      if (error && status !== 406) throw error;
+
+      console.log(data);
+
+      if (data) {
+        profile = { ...profile, ...data, last_updated: new Date()};
       }
+
+      console.log(profile);
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      loading = false;
     }
-  
-    async function updateProfile() {
-      try {
-        loading = true
-        const user = supabase.auth.user()
-  
-        const updates = {
-          id: user.id,
-          username,
-          website,
-          avatar_url,
-          updated_at: new Date(),
-        }
-  
-        let { error } = await supabase.from('profiles').upsert(updates, {
-          returning: 'minimal', // Don't return the value after inserting
-        })
-  
-        if (error) throw error
-      } catch (error) {
-        alert(error.message)
-      } finally {
-        loading = false
+  }
+
+  async function updateProfile() {
+    try {
+      loading = true;
+      const user = supabase.auth.user();
+
+      if (password.newPassword && password.newPassword == password.confirmPassword) {
+        let { error: updateError, ..._ } = await supabase.auth.update({
+          password: password.newPassword,
+        });
+        if (updateError) throw updateError;
       }
+
+      let { data: profileUpdate, error: profileError } = await supabase
+        .from("profiles")
+        .upsert(profile);
+      if (profileError) throw profileError;
+
+      console.log(profileUpdate);
+
+    } catch (error) {
+      alert(error.message);
+      console.log(error);
+    } finally {
+      loading = false;
     }
-  
-    async function signOut() {
-      try {
-        loading = true
-        let { error } = await supabase.auth.signOut()
-        if (error) throw error
-      } catch (error) {
-        alert(error.message)
-      } finally {
-        loading = false
-      }
-      
+  }
+
+  async function signOut() {
+    try {
+      loading = true;
+      let { error } = await supabase.auth.signOut();
+      if (error) throw error;
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      loading = false;
     }
-  </script>
-  
-  <form use:getProfile class="form-widget" on:submit|preventDefault={updateProfile}>
-    <div>
-      <label for="email">Email</label>
-      <input id="email" type="text" value={$user.email} disabled />
+  }
+</script>
+
+{#if $user}
+  <form use:getProfile on:submit|preventDefault={updateProfile}>
+    <div class="form-row">
+      <div class="form-element">
+        <label for="first name">First Name</label>
+        <input name="first name" type="text" bind:value={profile.firstName} />
+      </div>
+      <div class="form-element">
+        <label for="last name">Last Name</label>
+        <input name="last name" type="text" bind:value={profile.lastName} />
+      </div>
+      <div class="form-element">
+        <label for="birthday">birthday</label>
+        <input type="date" bind:value={profile.birthday} />
+      </div>
     </div>
-    <div>
-      <label for="username">Name</label>
-      <input
-        id="username"
-        type="text"
-        bind:value={username}
-      />
+    <div class="form-row">
+      <div class="form-element">
+        <label for="email">E-Mail</label>
+        <input name="email" type="text" bind:value={$user.email} disabled />
+      </div>
     </div>
-    <div>
-      <label for="website">Website</label>
-      <input
-        id="website"
-        type="website"
-        bind:value={website}
-      />
+    <div class="form-row">
+      <div class="form-element">
+        <label for="new password">New Password</label>
+        <input type="password" bind:value={password.newPassword} />
+      </div>
+      <div class="form-element">
+        <label for="confirm password"
+          >Confirm Password {password.newPassword === password.confirmPassword
+            ? "[matching]"
+            : "[mismatched]"}</label
+        >
+        <input type="password" bind:value={password.confirmPassword} />
+      </div>
     </div>
-  
-    <div>
-      <input type="submit" class="button block primary" value={loading ? 'Loading ...' : 'Update'} disabled={loading}/>
-    </div>
-  
-    <div>
-      <button class="button block" on:click={signOut} disabled={loading}>
-        Sign Out
-      </button>
+    <div class="form-row">
+      <div class="form-element">
+        <input type="submit" value={"Update Profile"} />
+      </div>
     </div>
   </form>
+{:else}
+  <p>We've a problem here...</p>
+{/if}
+
+<style>
+  .form-row {
+    margin-top: 12pt;
+    width: 100%;
+    display: flex;
+    gap: 12pt;
+    justify-content: space-between;
+  }
+  .form-row > .form-element {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    min-width: none;
+  }
+  @media all and (max-width: 500px) {
+    .form-row {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+    }
+  }
+</style>
